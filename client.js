2bb93fa36ae4b890c6dbbe428d8a0b6f601fed33
makeavish <makeavish786@gmail.com>
2023-01-03 19:31:25 +0530

    chore: update sleep to 50s to ensure all traces are exported

--- client.js
+++ client.js
@@ -39,8 +39,8 @@ function main() {
     // The process must live for at least the interval past any traces that
     // must be exported, or some risk being lost if they are recorded after the
     // last export.
-    console.log('Sleeping 5 seconds before shutdown to ensure all records are flushed.');
-    setTimeout(() => { console.log('Completed.'); }, 5000);
+    console.log('Sleeping 50 seconds before shutdown to ensure all records are flushed.');
+    setTimeout(() => { console.log('Completed.'); }, 50000);
 }
 
 main();
